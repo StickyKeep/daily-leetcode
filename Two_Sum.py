44071a09706c33331c8532013fd90ba7af6e11df
redpanda <33100474+StickyKeep@users.noreply.github.com>
2023-09-07 19:54:18 +0200

    Something something something

--- Two_Sum.py
+++ Two_Sum.py
@@ -29,7 +29,6 @@ class Solution:
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        self.nums = nums
         for i, value in enumerate(nums):
             for n, value2 in enumerate(nums):
                 if value + value2  == target and i != n:
