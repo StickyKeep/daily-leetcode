26bfb7a806bcd487883c9eaeb9ae6a03e5b93474
redpanda <33100474+StickyKeep@users.noreply.github.com>
2023-09-09 16:33:39 +0200

    First solution

---
+++ DP_509_fibonacci_number.py
@@ -0,0 +1,30 @@
+"""
+The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+F(0) = 0, F(1) = 1
+F(n) = F(n - 1) + F(n - 2), for n > 1.
+Given n, calculate F(n).
+https://leetcode.com/problems/fibonacci-number/description/?envType=study-plan-v2&envId=dynamic-programming
+"""
+
+# First solution: 
+
+class Solution:
+    def fib(self, n: int) -> int:
+        if n == 0:
+            return 0
+        if n == 1: 
+           return 1
+        if n == 2:
+           return 1
+        tab = [0] * n
+        tab[0] = 1
+        tab[1] = 1
+        for n in range(1, n):
+            tab[n] = tab[n-1] + tab[n-2]
+        return tab[n]
+    
+
+# Runtime 44ms
+# Beats 54.34% of users with Python3
+# Memory 16.36MB
+# Beats 22.23%of users with Python3
