c6ce6223705a89672f4dbbd2d11d8449424b1789
redpanda <33100474+StickyKeep@users.noreply.github.com>
2023-09-09 16:41:11 +0200

    First solution

---
+++ DP_1137_N-th_tribonacci_number.py
@@ -0,0 +1,32 @@
+"""
+The Tribonacci sequence Tn is defined as follows: 
+T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
+Given n, return the value of Tn.
+https://leetcode.com/problems/n-th-tribonacci-number/description/?envType=study-plan-v2&envId=dynamic-programming
+"""
+
+# First solution with modified code from 509. Fibonacci Number
+
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n == 0:
+            return 0
+        if n == 1: 
+            return 1
+        if n == 2:
+            return 1
+        if n == 3:
+            return 2
+            
+        tab = [0] * n
+        tab[0] = 1
+        tab[1] = 1
+        
+        for n in range(1, n):
+            tab[n] = tab[n-1] + tab[n-2] + tab[n-3]
+        return tab[n]
+        
+# Runtime 44ms
+# Beats 31.00%of users with Python3
+# Memory 16.16MB
+# Beats 90.48%of users with Python3
